aa8f8ba25b3a1ed96e2b5675226a4b9b4d94acd0
Siew Wing Fei <wingfei.siew@nova-hub.com>
2024-04-28 23:01:53 +0800

    k

--- app/models.py
+++ app/models.py
@@ -1,5 +1,21 @@
 from websocket_server import WebsocketServer
 from dataclasses import dataclass
+from pony.orm import *
+
+db = Database()
+db.bind(provider='sqlite', filename='../attdb.db', create_db=True)
+
+class Author(db.Entity):
+    name = Required(str)
+    books = Set('Book')
+
+class Book(db.Entity):
+    title = Required(str)
+    publication_date = Required(str)
+    isbn = Required(str)
+    author = Required(Author)
+    
+db.generate_mapping(create_tables=True)
 
 @dataclass
 class DeviceStatus:
@@ -13,3 +29,7 @@ class DeviceStatus:
     
     def __str__(self):
         return f'DeviceStatus [deviceSn={self.deviceSn}, webSocket={self.webSocket}, status={self.status}]'
+    
+# with db_session:
+#     author = Author(name='John Doe')
+#     book = Book(title='Python Programming', publication_date='2022-01-01', isbn='978-0-123456-78-9', author=author)
